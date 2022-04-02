45c6ab31f6710f8d5757993de181b3357eddf882
T0w3r <torres.alejo2001@gmail.com>
2022-04-02 15:36:43 -0300

    added db connection

---
+++ src/db.js
@@ -0,0 +1,16 @@
+
+const mongoose = require('mongoose')
+
+const connectDb = async  () =>   {
+  try {
+    mongoose.connect(process.env['DB_CONNECTION_STRING'], {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      
+    })
+    console.log('Connected to MongoDB')
+  } catch (error) {
+    console.log(error.message)
+  }
+}
+ module.exports = {connectDb}
