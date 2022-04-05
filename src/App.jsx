62ef8023f9ed321dc33c8bf05dd81f562ea1c1d5
T0w3r <torres.alejo2001@gmail.com>
2022-04-05 13:35:53 -0300

    init + testing real time response

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,21 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState,useEffect } from 'react'
+
+import socketIOClient from "socket.io-client";
+const ENDPOINT = "http://localhost:3000";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [response, setResponse] = useState({});
+  useEffect(() => {
+    const socket = socketIOClient(ENDPOINT);
+    socket.on("message", data => {
+      setResponse(data);
+    });
+  }, []);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div>
+      <h1>Sale el mensaje:</h1>
+      <h1>{response.message}</h1>
     </div>
   )
 }
