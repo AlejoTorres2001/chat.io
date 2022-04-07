c2831c3e20c13a43511bf29098083ddccb8cb550
T0w3r <torres.alejo2001@gmail.com>
2022-04-07 14:18:55 -0300

    extended colors from tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,20 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend:{
+      colors:{
+        'gray-light':'#202C33',
+        'gray-dark':'#182229',
+        'gray-darker':'#111B21',
+        'gray-selected':'#2A3942',
+        'gray-icons':'#AEBAC1',
+        'green-mssg':'#00A884',
+        'pink-chat':'#E26AB6',
+        'light-blue-chat':'#4C8F93',
+        'white-mssg':'#E9EDEF',
+        'gray-placeholder':'#8696A0',
+      },
+    }
   },
   plugins: [],
 }
