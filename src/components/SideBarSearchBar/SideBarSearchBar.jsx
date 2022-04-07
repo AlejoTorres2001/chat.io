0cbb36534a10edf3ab1b52cb624a12c8f96d3bec
T0w3r <torres.alejo2001@gmail.com>
2022-04-07 14:27:54 -0300

    added text ellipsis

--- src/components/SideBarSearchBar/SideBarSearchBar.jsx
+++ src/components/SideBarSearchBar/SideBarSearchBar.jsx
@@ -4,7 +4,7 @@ const SideBarSearchBar = () => {
     <div className="flex border-b border-gray-selected">
       <div className="flex justify-center my-2 mx-3 bg-gray-light w-full h-[35px] rounded-md ">
         <SearchIcon className="w-6 h-6 mt-1 ml-2 mr-5 text-gray-icons" />
-        <input type="text" className="box-border w-full overflow-clip  rounded-md bg-gray-light border-none outline-none placeholder-gray-placeholder" placeholder="Busca un chat o inicia uno nuevo" />
+        <input type="text" className="box-border w-full overflow-clip  rounded-md bg-gray-light border-none outline-none placeholder-gray-placeholder text-ellipsis text-white-mssg" placeholder="Busca un chat o inicia uno nuevo" />
       </div>
     </div>
   )
