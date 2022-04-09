a81e08f5edd6608941bbd26ff962f2e8bb23b22e
T0w3r <torres.alejo2001@gmail.com>
2022-04-08 22:26:52 -0300

    added backend endpoints references

---
+++ src/endpoints.js
@@ -0,0 +1,29 @@
+const url= process.env.NODE_ENV === 'development' ? 'http://localhost:3000': ''
+const routes =  {
+  URL:url,
+  chats:{
+    getChats: url+'/chats',
+    UsersChats: url+'/chats/:userId',
+    getChat: url+'/chats/:id',
+    createChat: url+'/chats',
+    updateChatName: url+'/chats/:id',
+    updateChatImage: url+'/chats/image/:id',
+    deleteChat: url+'/chats/delete/:id',
+  },
+  messages:{
+    createMessage: url+'/messages',
+    deleteMessages: url+'/messages/delete/:id',
+    getMessages: url+'/messages',
+    getUsersMessages: url+'/messages/:userId',
+  },
+  users:{
+    createUser: url+'/users',
+    updateUser: url+'/users/:id',
+    updateUserImage: url+'/users/image/:id',
+    deleteUser: url+'/users/delete/:id',
+    getUsers: url+'/users',
+    getUser: url+'/users/:id',
+  }
+
+}
+export default routes
