6d548fdf27eafcd78498b01e4f4bda5f950e5892
T0w3r <torres.alejo2001@gmail.com>
2022-04-13 12:08:01 -0300

    added atom for chats

---
+++ src/atoms/chatsAtom.js
@@ -0,0 +1,7 @@
+import { atom } from "recoil";
+
+const chatsState = atom({
+  key: "chatsState",
+  default:[]
+})
+export default chatsState
--- src/components/ChatList/ChatList.jsx
+++ src/components/ChatList/ChatList.jsx
@@ -1,32 +1,33 @@
-import {useState,useEffect} from 'react'
-import ChatItem from '../ChatItem/ChatItem';
-const userId='62489759e7cffb86b5ea0a86'
-import getUsersChats from '../../functions/getUsersChats';
-import socketIOClient from 'socket.io-client';
-import routes from '../../endpoints';
+import { useEffect } from "react";
+import ChatItem from "../ChatItem/ChatItem";
+import getUsersChats from "../../functions/getUsersChats";
+import socketIOClient from "socket.io-client";
+import routes from "../../endpoints";
 import { useRecoilState } from "recoil";
-import sessionState from '../../atoms/sessionAtom';
+import sessionState from "../../atoms/sessionAtom";
+import chatsState from "../../atoms/chatsAtom";
 
 const ChatList = () => {
-  const [chats, setChats] = useState([])
+  const [chats, setChats] = useRecoilState(chatsState);
   const [session, setSession] = useRecoilState(sessionState);
-  const addChat = (c) => setChats(prevC => [...prevC, c]);
-  const deleteChat = (c) => setChats(prevC => prevC.filter(chat => chat._id !== c._id));
+  const addChat = (c) => setChats((prevC) => [...prevC, c]);
+  const deleteChat = (c) =>
+    setChats((prevC) => prevC.filter((chat) => chat._id !== c._id));
 
   useEffect(() => {
-    getUsersChats(session.userId).then(chats => setChats(chats))
+    getUsersChats(session.id).then((chats) => setChats(chats));
     const socket = socketIOClient(routes.URL);
-    socket.on('newChat', addChat);
-    socket.on('deletedChat', deleteChat);
-    return () => socket.disconnect()
-  }, [])
+    socket.on("newChat", addChat);
+    socket.on("deletedChat", deleteChat);
+    return () => socket.disconnect();
+  }, []);
   return (
-    <div className='flex flex-col h-full'>
-    {chats.map(chat => {
-      return (<ChatItem key={chat._id} chat={chat}/>)
-    })}
+    <div className="flex flex-col h-full">
+      {chats.map((chat) => {
+        return <ChatItem key={chat._id} chat={chat} />;
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default ChatList
+export default ChatList;
