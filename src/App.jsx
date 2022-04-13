ad67195301485df81e98e4e123821c54cdd1c70f
T0w3r <torres.alejo2001@gmail.com>
2022-04-13 10:44:42 -0300

    persisting session between refreshes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,49 @@
-import { useState,useEffect } from 'react'
-import { useRecoilState } from 'recoil';
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { useRecoilState } from "recoil";
 import socketIOClient from "socket.io-client";
-import sessionState from './atoms/sessionAtom';
-import Chat from './components/Chat/Chat';
-import LogInForm from './components/LogInForm/LogInForm';
-import SideBar from './components/SideBar/SideBar';
+import sessionState from "./atoms/sessionAtom";
+import Chat from "./components/Chat/Chat";
+import LogInForm from "./components/LogInForm/LogInForm";
+import SideBar from "./components/SideBar/SideBar";
+import routes from "./endpoints";
+import validateSession from "./functions/validateSession";
 const ENDPOINT = "http://localhost:3000";
 function App() {
-  const [session,setSession] = useRecoilState(sessionState);
-  const isLoggedIn = () => !!session?.id
+  const [session, setSession] = useRecoilState(sessionState);
+  const [isLoggedIn, setIsLoggedIn] = useState(!!sessionState.id);
   const [response, setResponse] = useState({});
   useEffect(() => {
     const socket = socketIOClient(ENDPOINT);
-    socket.on("message", data => {
+    socket.on("message", (data) => {
       setResponse(data);
     });
   }, []);
-  console.log(response)
+  useEffect(() => {
+    validateSession()
+      .then((data) => {
+        if (data.auth) {
+          setSession(data.user);
+          setIsLoggedIn(true);
+          return;
+        }
+      })
+      .catch((err) => {
+        setIsLoggedIn(false);
+      });
+  }, []);
   return (
     <>
-    {isLoggedIn() ? (
-      <div className='grid grid-cols-[0.3fr,1fr]'>
-      <SideBar></SideBar>
-      <Chat></Chat>
-    </div>
-    ) :(
-        <LogInForm/>
-    )} 
+      {isLoggedIn ? (
+        <div className="grid grid-cols-[0.3fr,1fr]">
+          <SideBar></SideBar>
+          <Chat></Chat>
+        </div>
+      ) : (
+        <LogInForm setIsLoggedIn={setIsLoggedIn} />
+      )}
     </>
-    
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/LogInForm/LogInForm.jsx
+++ src/components/LogInForm/LogInForm.jsx
@@ -1,48 +1,48 @@
-import {useState,useEffect} from 'react'
-import logIn from '../../functions/logIn';
-import sessionState from '../../atoms/sessionAtom';
-import { useRecoilState } from 'recoil';
-import singUp from '../../functions/signUp';
+import { useState, useEffect } from "react";
+import logIn from "../../functions/logIn";
+import sessionState from "../../atoms/sessionAtom";
+import { useRecoilState } from "recoil";
+import singUp from "../../functions/signUp";
 
-const LogInForm = () => {
+const LogInForm = ({ setIsLoggedIn }) => {
   const [isRegistration, setIsRegistration] = useState(false);
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [session,setSession] = useRecoilState(sessionState);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [session, setSession] = useRecoilState(sessionState);
   const [errors, setErrors] = useState([]);
   const [success, setSuccess] = useState(false);
   useEffect(() => {
     const time = setTimeout(() => {
       setErrors([]);
       setSuccess(false);
-    },3000)
+    }, 3000);
     return () => clearTimeout(time);
   }, [errors, success]);
   const handleLogin = async (e) => {
     e.preventDefault();
     const response = await logIn(username, password);
-    if(response?.data?.auth){
+    if (response?.data?.auth) {
       setSession(response?.data?.user);
+      setIsLoggedIn(true);
       setSuccess(false);
-      return
+      return;
     }
-    setErrors([...errors,"Invalid username or password"]);
-
-}
-const handleRegister = async (e) => {
-  e.preventDefault();
-  const response = await singUp(username, password);
-  if(response?.correct){
-    setIsRegistration(false);
-    setSuccess(true);
-    return
-  }
-  setErrors([...errors,response.error]);
-};
+    setErrors([...errors, "Invalid username or password"]);
+  };
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    const response = await singUp(username, password);
+    if (response?.correct) {
+      setIsRegistration(false);
+      setSuccess(true);
+      return;
+    }
+    setErrors([...errors, response.error]);
+  };
   return (
     <div className="flex items-center  justify-center bg-gray-darker h-screen ">
-          <div className="flex flex-col align-middle   bg-gray-dark w-1/2 h-1/2 rounded-md">
-          {errors.length > 0 && (
+      <div className="flex flex-col align-middle   bg-gray-dark w-1/2 h-1/2 rounded-md">
+        {errors.length > 0 && (
           <div className="flex flex-col items-center bg-gray-selected text-red-600 rounded-md">
             {errors.map((error, index) => {
               return <p key={index}>{error}</p>;
@@ -54,12 +54,22 @@ const handleRegister = async (e) => {
             <p>User succesfully created</p>
           </div>
         )}
-            <div className="  mx-4 flex flex-col align-middle justify-center mt-[20%] lg:mt-24 ">
-            <h2 className='-mb-8 lg:-mb-7 text-white-mssg'>Username</h2>
-            <input type="text" className='w-full h-8 bg-gray-light my-8 text-white-mssg rounded-md' value={username} onChange={(e)=>setUsername(e.target.value)} />
-            <h2 className='-mb-8 lg:-mb-7 text-white-mssg'>Password</h2>
-            <input type="password" className='w-full h-8 bg-gray-light my-8 text-white-mssg rounded-md' value={password} onChange={(e)=>setPassword(e.target.value)} />
-            {isRegistration ? (
+        <div className="  mx-4 flex flex-col align-middle justify-center mt-[20%] lg:mt-24 ">
+          <h2 className="-mb-8 lg:-mb-7 text-white-mssg">Username</h2>
+          <input
+            type="text"
+            className="w-full h-8 bg-gray-light my-8 text-white-mssg rounded-md"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <h2 className="-mb-8 lg:-mb-7 text-white-mssg">Password</h2>
+          <input
+            type="password"
+            className="w-full h-8 bg-gray-light my-8 text-white-mssg rounded-md"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          {isRegistration ? (
             <button
               className="w-full h-8 bg-green-mssg my-4 text-white-mssg  rounded-md"
               onClick={handleRegister}
@@ -74,9 +84,9 @@ const handleRegister = async (e) => {
               Log In
             </button>
           )}
-            </div>
-            <div className="flex mt-14 justify-center items-center whitespace-nowrap text-sm lg:text-base">
-            {isRegistration ? (
+        </div>
+        <div className="flex mt-14 justify-center items-center whitespace-nowrap text-sm lg:text-base">
+          {isRegistration ? (
             <p className="text-white-mssg">
               Already have an account?{" "}
               <span
@@ -97,13 +107,10 @@ const handleRegister = async (e) => {
               </span>
             </p>
           )}
-            </div>
-            
-        
-            
-          </div>
         </div>
-  )
-}
+      </div>
+    </div>
+  );
+};
 
-export default LogInForm
+export default LogInForm;
