90aa0baead15c24b527eef66cc18e84f1684f2ba
T0w3r <torres.alejo2001@gmail.com>
2022-04-13 10:09:37 -0300

    isloggedin

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState,useEffect } from 'react'
+import { useRecoilState } from 'recoil';
 import socketIOClient from "socket.io-client";
+import sessionState from './atoms/sessionAtom';
 import Chat from './components/Chat/Chat';
 import LogInForm from './components/LogInForm/LogInForm';
 import SideBar from './components/SideBar/SideBar';
 const ENDPOINT = "http://localhost:3000";
-const isLoggedIn = false
 function App() {
+  const [session,setSession] = useRecoilState(sessionState);
+  const isLoggedIn = () => !!session?.id
   const [response, setResponse] = useState({});
   useEffect(() => {
     const socket = socketIOClient(ENDPOINT);
@@ -16,7 +19,7 @@ function App() {
   console.log(response)
   return (
     <>
-    {isLoggedIn ? (
+    {isLoggedIn() ? (
       <div className='grid grid-cols-[0.3fr,1fr]'>
       <SideBar></SideBar>
       <Chat></Chat>
