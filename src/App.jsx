4e2e0caa9225d931ee835f3439f0961f5243b292
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-05-13 19:01:23 -0500

    removed unused useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import { useRecoilState } from "recoil";
-import socketIOClient from "socket.io-client";
 import sessionState from "./atoms/sessionAtom";
 import ChatBody from "./components/ChatBody/ChatBody";
 import ChatContainer from "./components/ChatContainer/ChatContainer";
@@ -8,18 +7,10 @@ import ChatHeader from "./components/ChatHeader/ChatHeader";
 import ChatInputBar from "./components/ChatInputBar/ChatInputBar";
 import LogInForm from "./components/LogInForm/LogInForm";
 import SideBar from "./components/SideBar/SideBar";
-import routes from "./endpoints";
 import validateSession from "./functions/validateSession";
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(!!sessionState.id);
-  const [response, setResponse] = useState({});
   const [session, setSession] = useRecoilState(sessionState);
-  useEffect(() => {
-    const socket = socketIOClient(routes.URL);
-    socket.on("message", (data) => {
-      setResponse(data);
-    });
-  }, []);
   useEffect(() => {
     validateSession()
       .then((data) => {
@@ -39,9 +30,9 @@ function App() {
         <div className="grid grid-cols-[0.3fr,1fr]">
           <SideBar></SideBar>
           <ChatContainer>
-            <ChatHeader/>
-            <ChatBody/>
-            <ChatInputBar/>
+            <ChatHeader />
+            <ChatBody />
+            <ChatInputBar />
           </ChatContainer>
         </div>
       ) : (
