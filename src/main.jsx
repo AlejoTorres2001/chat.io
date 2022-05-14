713c19a6a00f725815a170f72a2395ea318121bf
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-05-13 19:01:53 -0500

    made a Socket provider

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import './main.css'
 import {RecoilRoot} from "recoil";
+import { SocketProvider } from './providers/SocketProvider';
 ReactDOM.render(
   <React.StrictMode>
+    <SocketProvider>
     <RecoilRoot>
     <App />
     </RecoilRoot>
+    </SocketProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
---
+++ src/providers/SocketProvider.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState, createContext } from "react";
+import { io } from "socket.io-client";
+import routes from "../endpoints";
+
+const webSocket = new io(routes.URL);
+const SOCKET_RECONNECTION_TIMEOUT = 5000;
+export const SocketContext = createContext(webSocket);
+
+export const SocketProvider = (props) => {
+  const [ws, setWs] = useState(webSocket);
+
+  useEffect(() => {
+    const onClose = () => {
+      setTimeout(() => {
+        setWs(io(routes.URL));
+      }, SOCKET_RECONNECTION_TIMEOUT);
+    };
+
+    ws.on("close", onClose);
+
+    return () => {
+      ws.off("close", onClose);
+    };
+  }, [ws, setWs]);
+
+  return (
+    <SocketContext.Provider value={ws}>{props.children}</SocketContext.Provider>
+  );
+};
