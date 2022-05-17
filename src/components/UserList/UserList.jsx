53f977ea58a15426a2316333fe4932feaf567f70
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-05-16 22:04:37 -0500

    avoid showing self as another user

--- src/components/UserList/UserList.jsx
+++ src/components/UserList/UserList.jsx
@@ -1,12 +1,16 @@
 import { useState, useEffect } from "react";
+import { useRecoilState } from "recoil";
+import sessionState from "../../atoms/sessionAtom";
 import getUsers from "../../functions/getUsers";
 import UserItem from "../UserItem/UserItem";
 
 const UserList = () => {
   const [users, setUsers] = useState([]);
+  const [session, setSession] = useRecoilState(sessionState);
+
   useEffect(() => {
     getUsers().then((users) => {
-      setUsers(users);
+      setUsers(users.filter((user) => user._id !== session.id));
     });
   }, []);
 
